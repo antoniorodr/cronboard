ae85b801609d3d4e3daff1db36622ba9f3c529f3
Antonio Rodriguez <102554647+antoniorodr@users.noreply.github.com>
2026-09-14 10:17:28 +0200

    test(fixture): make fixture for app async

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,6 @@ from cronboard.app import CronBoard
 
 
 @pytest.fixture
-def app() -> types.GeneratorType:
+async def app() -> types.AsyncGeneratorType:
     cronboard_app: CronBoard = CronBoard()
     yield cronboard_app
